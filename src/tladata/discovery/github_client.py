0ffd08e78cdbde9d97c6b2450cc3df9d009f989a
bortiz-101 <bortiz101@hotmail.com>
2026-02-26 21:38:25 -0600

    adds GitHub client class, a wrapper for GitHub API

---
+++ src/tladata/discovery/github_client.py
@@ -0,0 +1,17 @@
+from typing import Any, Dict, Optional, cast
+import requests
+
+
+class GithubClient:
+    def __init__(self, token: str) -> None:
+        self.base_url: str = "https://api.github.com"
+        self.headers: Dict[str, str] = {
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/vnd.github+json",
+        }
+
+    def get(self, path: str, params: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+        url = f"{self.base_url}{path}"
+        resp = requests.get(url, headers=self.headers, params=params)
+        resp.raise_for_status()
+        return cast(Dict[str, Any], resp.json())
