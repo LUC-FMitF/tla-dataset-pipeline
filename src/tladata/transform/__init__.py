693baee9f26d1159701285edd7a1eec8e503d177
bortiz-101 <bortiz101@hotmail.com>
2026-05-10 20:10:16 -0500

    Remove deprecated transform module and nightly parse workflow

--- src/tladata/transform/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Transform module for TLA+ feature extraction and format conversion."""
-
-from tladata.transform.json import TlaTransformer, transform
-
-__all__ = ["TlaTransformer", "transform"]
